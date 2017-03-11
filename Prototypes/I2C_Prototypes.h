c6431c3f0863d10e54511c41479ee62ecee48e0d
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-11 16:12:19 +0200

    Update I2C_Prototypes.h

--- Prototypes/I2C_Prototypes.h
+++ Prototypes/I2C_Prototypes.h
@@ -3,20 +3,20 @@
 #define FUNCTION_PROTOTYPES_H_
 #include "../Datatypes/DataTypes.h"
 
-void I2C_Start_Condition ( void );
-void I2C_Stop_Condition  ( void );
+void I2C_Start_Condition        ( void );
+void I2C_Stop_Condition         ( void );
 
-u8   I2C_Status( void );
-void I2C_Address_Set( u8 Address );
+u8   I2C_Status                 ( void );
+void I2C_Address_Set            ( u8 Address );
 
-void I2C_WriteTo_Address ( u8 Address );
-void I2C_Write ( u8 Data );
+void I2C_WriteTo_Address        ( u8 Address );
+void I2C_Write                  ( u8 Data );
 
-void I2C_ReadFROM_Address ( u8 Address );
+void I2C_ReadFROM_Address       ( u8 Address );
 u8   I2C_Read ( void );
-u8   I2C_Read_With_ACK ( u8 ACK );
+u8   I2C_Read_With_ACK          ( u8 ACK );
 
-void I2C_Restart( void );
+void I2C_Restart                ( void );
 
 
-#endif /* FUNCTION_PROTOTYPES_H_ */
+#endif /* FUNCTION_PROTOTYPES_H_ */   
