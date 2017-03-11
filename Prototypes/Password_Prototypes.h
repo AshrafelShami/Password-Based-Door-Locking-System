767aa7592cb2bdadaea1316e4e744e67318c4b51
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-11 16:10:41 +0200

    Update Password_Prototypes.h

--- Prototypes/Password_Prototypes.h
+++ Prototypes/Password_Prototypes.h
@@ -3,7 +3,7 @@
 #define PASSWORD_PROTOTYPES_H_
 #include "../Datatypes/DataTypes.h"
 
-u8 *  Password_Enter ( void );
-void User_Login ( void );
+u8 * Password_Enter     ( void );
+void User_Login         ( void );
 
 #endif /* PASSWORD_PROTOTYPES_H_ */
