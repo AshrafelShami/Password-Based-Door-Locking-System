2da73eb473bb5caeda8d793b19c0d4992791ef06
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-11 16:14:19 +0200

    Update Password_Config.h

--- PW_Module/Password_Config.h
+++ PW_Module/Password_Config.h
@@ -11,7 +11,7 @@
 #include <avr/io.h>
 #define LED_Green_On	PORTB |= (1U << 2)
 #define LED_Green_OFF	PORTB &=~(1U << 2)
-#define LED_Red_On		PORTB |= (1U << 5)
-#define LED_Red_OFF		PORTB &=~(1U << 5)
+#define LED_Red_On	PORTB |= (1U << 5)
+#define LED_Red_OFF	PORTB &=~(1U << 5)
 
 #endif /* PASSWORD_CONFIG_H_ */
