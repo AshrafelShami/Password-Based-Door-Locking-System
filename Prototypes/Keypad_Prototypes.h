14932bdbab8ec1daa2c7bb2d44d621bfbb2573ed
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-11 16:11:32 +0200

    Update Keypad_Prototypes.h

--- Prototypes/Keypad_Prototypes.h
+++ Prototypes/Keypad_Prototypes.h
@@ -4,8 +4,8 @@
 #include "../Datatypes/DataTypes.h"
 
 void Keypad_Init(vu8* ROW, vu8* LINE,
-u8 ROW0, u8 ROW1, u8 ROW2, u8 ROW3,
-u8 LIN0, u8 LIN1, u8 LIN2, u8 LIN3);
+                  u8 ROW0, u8 ROW1, u8 ROW2, u8 ROW3,
+                  u8 LIN0, u8 LIN1, u8 LIN2, u8 LIN3);
 
 u8 Keypad_Scan  ( void );
 
