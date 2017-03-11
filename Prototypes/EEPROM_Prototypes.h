821b4ebfabf13d5ec903d91b755db8b331ce5174
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-11 16:09:16 +0200

    Update EEPROM_Prototypes.h

--- Prototypes/EEPROM_Prototypes.h
+++ Prototypes/EEPROM_Prototypes.h
@@ -3,8 +3,8 @@
 #define EEPROM_PROTOTYPES_H_
 #include "../Datatypes/DataTypes.h"
 
-u8	EEPROM_Read			( u8 Memory_Address );
-u8	EEPROM_Write		( u8 Data, u8 Memory_Address );
+u8	EEPROM_Read	    ( u8 Memory_Address );
+u8	EEPROM_Write	    ( u8 Data, u8 Memory_Address );
 u8	EEPROM_Write_String ( u8 * Data, u8 Memory_Address );
 
 #endif /* EEPROM_PROTOTYPES_H_ */
